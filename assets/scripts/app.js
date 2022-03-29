f0cf0a31cb0e59e88aecc8cb69b7c5c62da59018
Mahmoud El Helou <mahelhelou@gmail.com>
2022-03-29 13:11:25 +0300

    Update 2022-03-29T10:11:13.401Z

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,7 +34,7 @@ jQuery(function ($) {
 	})
 
 	// Stores owl carousel
-	$('.stores .owl-carousel').owlCarousel({
+	$('.authors .owl-carousel').owlCarousel({
 		loop: true,
 		nav: true,
 		// navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
